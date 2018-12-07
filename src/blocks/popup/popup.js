79e6b8abb525e03917ee4863b9a8192f1a0534f9
earthaddicted <elenika.om@gmail.com>
2018-12-07 14:59:51 +0200

    add popup form

---
+++ src/blocks/popup/popup.js
@@ -0,0 +1,31 @@
+  (function () {
+    var wrapPopup = document.querySelector('.popup-block');
+    var popup = document.querySelector('.popup-block');
+    var close = document.querySelector('.close');
+    var prompt = document.querySelector('.order-individual');
+    var body = document.querySelector('body');
+
+    if (prompt) {
+    prompt.addEventListener('click', showPopup);
+
+        function showPopup() {
+            popup.classList.add('visible-popup');
+            body.classList.add('no-scroll');
+        }
+
+        close.addEventListener('click', hidePopup);
+
+        function hidePopup() {
+            popup.classList.remove('visible-popup');
+            body.classList.remove('no-scroll');
+        }
+
+        window.addEventListener('click', removePopup);
+          function removePopup(e) {
+            var target = e.target;
+            if (target == wrapPopup) {
+                hidePopup();
+            }
+          }
+      }
+  })();
