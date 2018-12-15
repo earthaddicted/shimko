ab14ca29c425b434beb11e3c238f9dc066a07fe1
earthaddicted <elenika.om@gmail.com>
2018-12-15 19:58:41 +0200

    add page blog

--- build/assets/app.js
+++ build/assets/app.js
@@ -1,3 +1,68 @@
+// $('.blog-block').masonry({
+//   columnWidth: 445,
+//   itemSelector: '.grid'
+// });
+
+$('.blog-block').masonry({
+    // options
+    itemSelector : '.blog-block__item',
+    // columnWidth: 465,
+  	fitWidth: true
+    // console.log('masonry');
+    // isAnimated: !Modernizr.csstransitions
+});
+
+
+
+
+// (function() {
+//   var grid = $('.masonry-grid-item').imagesLoaded(function() {
+//     $('.masonry-grid').masonry({
+//       itemSelector: '.masonry-grid-item'
+//     });
+//   });
+// })();
+// $('.hamburger__icon').click(function() {
+//   $(this).toggleClass('open');
+//   $('header').toggleClass('open');
+// });
+
+
+// $(".hamburger__icon").click(function(){
+// 	$(this).toggleClass('open');
+//     // $(this).next(".nav").slideToggle();
+//     $('.nav').slideToggle('slow');
+//     $('header').toggleClass('open');
+// });
+
+(function() {
+    var McButton = $("[data=hamburger-menu]");
+    var McBar1 = $(".bar-1");
+    var McBar2 = $(".bar-2");
+    var McBar3 = $(".bar-3");
+    var body = $("body");
+
+
+    McButton.click(function() {
+        $(this).toggleClass("active");
+
+        if (McButton.hasClass("active")) {
+            McBar1.velocity({ top: "50%" }, { duration: 100, easing: "swing" });
+            McBar3.velocity({ top: "50%" }, { duration: 100, easing: "swing" })
+                .velocity({ rotateZ: "90deg" }, { duration: 400, delay: 100, easing: [500, 20] });
+            McButton.velocity({ rotateZ: "135deg" }, { duration: 400, delay: 100, easing: [500, 20] });
+        } else {
+            McButton.velocity("reverse");
+            McBar3.velocity({ rotateZ: "0deg" }, { duration: 400, easing: [500, 20] })
+                .velocity({ top: "100%" }, { duration: 100, easing: "swing" });
+            McBar1.velocity("reverse", { delay: 400 });
+        }
+
+        $('header .nav').slideToggle('slow');
+        $('header').toggleClass('open');
+        body.toggleClass('no-scroll');
+    });
+})();
 $(window).on('load resize scroll',function(){
     if ($(window).width() < 1025) {
 
@@ -167,47 +232,6 @@ function initMap() {
   // });
 
 };
-// $('.hamburger__icon').click(function() {
-//   $(this).toggleClass('open');
-//   $('header').toggleClass('open');
-// });
-
-
-// $(".hamburger__icon").click(function(){
-// 	$(this).toggleClass('open');
-//     // $(this).next(".nav").slideToggle();
-//     $('.nav').slideToggle('slow');
-//     $('header').toggleClass('open');
-// });
-
-(function() {
-    var McButton = $("[data=hamburger-menu]");
-    var McBar1 = $(".bar-1");
-    var McBar2 = $(".bar-2");
-    var McBar3 = $(".bar-3");
-    var body = $("body");
-
-
-    McButton.click(function() {
-        $(this).toggleClass("active");
-
-        if (McButton.hasClass("active")) {
-            McBar1.velocity({ top: "50%" }, { duration: 100, easing: "swing" });
-            McBar3.velocity({ top: "50%" }, { duration: 100, easing: "swing" })
-                .velocity({ rotateZ: "90deg" }, { duration: 400, delay: 100, easing: [500, 20] });
-            McButton.velocity({ rotateZ: "135deg" }, { duration: 400, delay: 100, easing: [500, 20] });
-        } else {
-            McButton.velocity("reverse");
-            McBar3.velocity({ rotateZ: "0deg" }, { duration: 400, easing: [500, 20] })
-                .velocity({ top: "100%" }, { duration: 100, easing: "swing" });
-            McBar1.velocity("reverse", { delay: 400 });
-        }
-
-        $('header .nav').slideToggle('slow');
-        $('header').toggleClass('open');
-        body.toggleClass('no-scroll');
-    });
-})();
 (function() {
     // var wrapPopup = document.querySelector('.popup-block');
     var popup = document.querySelector('.popup-block');
