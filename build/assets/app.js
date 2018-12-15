f4ad5e61ea060fca54bd7995976317bfa8476719
earthaddicted <elenika.om@gmail.com>
2018-12-15 21:07:15 +0200

    add page blog article

--- build/assets/app.js
+++ build/assets/app.js
@@ -1,27 +1,8 @@
-// $('.blog-block').masonry({
-//   columnWidth: 445,
-//   itemSelector: '.grid'
-// });
 
 $('.blog-block').masonry({
-    // options
     itemSelector : '.blog-block__item',
-    // columnWidth: 465,
   	fitWidth: true
-    // console.log('masonry');
-    // isAnimated: !Modernizr.csstransitions
 });
-
-
-
-
-// (function() {
-//   var grid = $('.masonry-grid-item').imagesLoaded(function() {
-//     $('.masonry-grid').masonry({
-//       itemSelector: '.masonry-grid-item'
-//     });
-//   });
-// })();
 // $('.hamburger__icon').click(function() {
 //   $(this).toggleClass('open');
 //   $('header').toggleClass('open');
--- src/blocks/blog/blog.js
+++ src/blocks/blog/blog.js
@@ -1,24 +1,5 @@
-// $('.blog-block').masonry({
-//   columnWidth: 445,
-//   itemSelector: '.grid'
-// });
 
 $('.blog-block').masonry({
-    // options
     itemSelector : '.blog-block__item',
-    // columnWidth: 465,
   	fitWidth: true
-    // console.log('masonry');
-    // isAnimated: !Modernizr.csstransitions
 });
-
-
-
-
-// (function() {
-//   var grid = $('.masonry-grid-item').imagesLoaded(function() {
-//     $('.masonry-grid').masonry({
-//       itemSelector: '.masonry-grid-item'
-//     });
-//   });
-// })();
