1c1e8ddfbf81aff78a43b0aea6a4136e563f20a0
earthaddicted <elenika.om@gmail.com>
2018-12-16 01:26:25 +0200

    fix top img for blog icon

--- src/blocks/blog/blog.js
+++ src/blocks/blog/blog.js
@@ -10,25 +10,23 @@ $('.blog-block').masonry({
 (function() {
 	if ($('.blog-block__item').length) {
 		var tlAppearItem = new TimelineMax()
-		.fromTo('.blog-block__img img', 4, { y:0}, { y:-100})
+		.fromTo('.blog-block__img img', 4, { y:0}, { y:-110})
 
 		var controller = new ScrollMagic.Controller();
 
 		new ScrollMagic.Scene({
-				triggerElement: ".blog-block__img img",
-				duration: "300%",
-				// triggerHook: 1
-				triggerHook: 1,
-			})
+			triggerElement: ".blog-block__img img",
+			duration: "300%",
+			// triggerHook: 1
+			triggerHook: 1,
+		})
 
-			.setTween(
-				tlAppearItem
-				)
-
-				// .addIndicators({name:"paralax"})
-				.addTo(controller);
-			}
-
+		.setTween(
+			tlAppearItem
+			)
+			// .addIndicators({name:"paralax"})
+			.addTo(controller);
+		}
 })();
 
 /* ========== parallax END ==========  */
