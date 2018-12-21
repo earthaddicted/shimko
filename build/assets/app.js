231900d2fc05afb5a5341cbcd9ae84e5cf1a1402
earthaddicted <elenika.om@gmail.com>
2018-12-21 13:35:48 +0200

    add submenu to menu

--- build/assets/app.js
+++ build/assets/app.js
@@ -1,3 +1,35 @@
+
+
+$('.blog-block').masonry({
+    itemSelector : '.blog-block__item',
+  	fitWidth: true
+});
+
+
+/* ========== parallax ==========  */
+(function() {
+	if ($('.blog-block__item').length) {
+		var tlAppearItem = new TimelineMax()
+		.fromTo('.blog-block__img img', 4, { y:0}, { y:-250})
+
+		var controller = new ScrollMagic.Controller();
+
+		new ScrollMagic.Scene({
+			triggerElement: ".blog-block__img img",
+			duration: "300%",
+			// triggerHook: 1
+			triggerHook: 1,
+		})
+
+		.setTween(
+			tlAppearItem
+			)
+			.addIndicators({name:"paralax"})
+			.addTo(controller);
+		}
+})();
+
+/* ========== parallax END ==========  */
 // $('.hamburger__icon').click(function() {
 //   $(this).toggleClass('open');
 //   $('header').toggleClass('open');
@@ -39,38 +71,6 @@
         body.toggleClass('no-scroll');
     });
 })();
-
-
-$('.blog-block').masonry({
-    itemSelector : '.blog-block__item',
-  	fitWidth: true
-});
-
-
-/* ========== parallax ==========  */
-(function() {
-	if ($('.blog-block__item').length) {
-		var tlAppearItem = new TimelineMax()
-		.fromTo('.blog-block__img img', 4, { y:0}, { y:-250})
-
-		var controller = new ScrollMagic.Controller();
-
-		new ScrollMagic.Scene({
-			triggerElement: ".blog-block__img img",
-			duration: "300%",
-			// triggerHook: 1
-			triggerHook: 1,
-		})
-
-		.setTween(
-			tlAppearItem
-			)
-			.addIndicators({name:"paralax"})
-			.addTo(controller);
-		}
-})();
-
-/* ========== parallax END ==========  */
 $(window).on('load resize scroll',function(){
     if ($(window).width() < 1261) {
 
