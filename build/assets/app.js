8115e6245720a3bbdc42044416fb4d308dae2902
earthaddicted <elenika.om@gmail.com>
2018-12-21 15:05:19 +0200

    fix dropdown header menu

--- build/assets/app.js
+++ build/assets/app.js
@@ -30,6 +30,33 @@ $('.blog-block').masonry({
 })();
 
 /* ========== parallax END ==========  */
+$(window).on('load resize scroll',function(){
+    if ($(window).width() < 1261) {
+
+        if (window.pageYOffset > 200) {
+            $('header .logo').css({'background': 'rgba(255, 255, 255, .95)'});
+        }
+        else {
+            $('header .logo').css({'background': 'rgba(255, 255, 255, .3)'});
+        }
+    }  else if ($(window).width() < 480) {
+         if (window.pageYOffset > 30) {
+            $('header .logo').css({'background': 'rgba(255, 255, 255, .8)'});
+        }
+        else {
+            $('header .logo').css({'background': 'rgba(255, 255, 255, .3)'});
+        }
+
+    }   else {
+            $('header .logo').css({'background': 'rgba(255, 255, 255, 0)'});
+    }
+
+    if (window.pageYOffset > 100) {
+        $('.header').css({'background': 'rgba(255, 255, 255, .8)'});
+    } else {
+        $('.header').css({'background': 'rgba(255, 255, 255, .1)'});
+    }
+});
 // $('.hamburger__icon').click(function() {
 //   $(this).toggleClass('open');
 //   $('header').toggleClass('open');
@@ -71,33 +98,6 @@ $('.blog-block').masonry({
         body.toggleClass('no-scroll');
     });
 })();
-$(window).on('load resize scroll',function(){
-    if ($(window).width() < 1261) {
-
-        if (window.pageYOffset > 200) {
-            $('header .logo').css({'background': 'rgba(255, 255, 255, .95)'});
-        }
-        else {
-            $('header .logo').css({'background': 'rgba(255, 255, 255, .3)'});
-        }
-    }  else if ($(window).width() < 480) {
-         if (window.pageYOffset > 30) {
-            $('header .logo').css({'background': 'rgba(255, 255, 255, .8)'});
-        }
-        else {
-            $('header .logo').css({'background': 'rgba(255, 255, 255, .3)'});
-        }
-
-    }   else {
-            $('header .logo').css({'background': 'rgba(255, 255, 255, 0)'});
-    }
-
-    if (window.pageYOffset > 100) {
-        $('.header').css({'background': 'rgba(255, 255, 255, .8)'});
-    } else {
-        $('.header').css({'background': 'rgba(255, 255, 255, .1)'});
-    }
-});
 function initMap() {
   // $(document).ready(function() {
     /* google maps -----------------------------------------------------*/
