48e8ef63d15fd52c65b4a218c7a09a126497dc77
earthaddicted <elenika.om@gmail.com>
2019-01-09 17:07:25 +0200

    add gallery to portfolio page

---
+++ src/blocks/logo/logo (1).js
@@ -0,0 +1,27 @@
+$(window).on('load resize scroll',function(){
+    if ($(window).width() < 1025) {
+
+        if (window.pageYOffset > 200) {
+            $('header .logo').css({'background': 'rgba(255, 255, 255, .95)'});
+        }
+        else {
+            $('header .logo').css({'background': 'rgba(255, 255, 255, .3)'});
+        }
+    }  else if ($(window).width() < 480) {
+         if (window.pageYOffset > 30) {
+            $('header .logo').css({'background': 'rgba(255, 255, 255, .8)'});
+        }
+        else {
+            $('header .logo').css({'background': 'rgba(255, 255, 255, .3)'});
+        }
+
+    }   else {
+            $('header .logo').css({'background': 'rgba(255, 255, 255, 0)'});
+    }
+
+    if (window.pageYOffset > 100) {
+        $('.header').css({'background': 'rgba(255, 255, 255, .8)'});
+    } else {
+        $('.header').css({'background': 'rgba(255, 255, 255, .1)'});
+    }
+});
